61cad95b4fb7ed856f5745ee3916280a31a7204a
Sinthorn Pradutnam <psinthorn@gmail.com>
2018-12-28 14:53:07 +0700

    2nd commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send({ say: "Hello" });
+  res.status(200).send({ say: "Hello to 2nd Edited" });
 });
 
 const PORT = process.env.PORT || 8008;
